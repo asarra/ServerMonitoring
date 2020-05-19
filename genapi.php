ec8f484be46f74f7df19c349e6684ca7d91fbb04
asarra <20255127+asarra@users.noreply.github.com>
2020-05-19 23:21:51 +0200

    Update genapi.php

--- genapi.php
+++ genapi.php
@@ -1,7 +1,4 @@
 <?php
-//chmod 0777 html - when can't create file
-
-/*$servername = "localhost";//$username = "root";//$password = "line";$username = "test";$password = "lol";$db="quickmafs";$getQuery="select counter from api;";//$postQuery="insert into api(counter) values (1);";//$result = $conn->query($postQuery);$result = mysqli_query($conn,"SELECT * FROM api");//$conn->query($getQuery);echo ("$result");//echo "Connected successfully";*/
 if(!file_exists("api.txt")){fopen("api.txt","w+") or die("Unable to load!");}
 $txt_rw=fopen("api.txt","a+") or die("Unable to load!");
 $data = explode(" ", fgets($txt_rw));
